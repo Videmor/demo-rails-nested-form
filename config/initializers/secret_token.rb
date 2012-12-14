521f240eac5bbb2c699c553770c95de5c603674d
videmor <videmor@gmail.com>
2012-12-13 20:52:36 -0500

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NestedForm::Application.config.secret_token = '7e170be01eedbc762b4606408223d9f1b796273631a1c4befdd3f0aa620a5134ce0532324904a0fad90d96606835d7d07233967267e3ce8dfbddf50ce1617985'
